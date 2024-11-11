1a1a4519a1dfc9bf075071a6b794332ee62bc5d0
suhasjohn96 <suhasjohn96@gmail.com>
2024-11-11 12:13:41 +0000

    clear understanding of 02_variables

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,16 @@
+const accountId =144553                                             // value cant be changed
+let accountEmail = "suhas@gmail.com"                                // recent update on scope
+var accountPassword = "12345"                                       // not suggested to use now bcz of block scope and function scope
+accountCity="Mysore"                                                // possible to declare a variable to declare like this but strictly not recommended
+
+// accountId=2   
+accountEmail="dsfsd@gmail.com"
+accountPassword="123654"
+accountCity="Bengaluru"
+
+let marks;
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,marks]);

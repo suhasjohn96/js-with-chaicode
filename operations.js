e7eed8d2c2b40afba14b5957856af87895326193
suhasjohn96 <suhasjohn96@gmail.com>
2024-12-28 07:03:06 +0000

    added operations file

---
+++ operations.js
@@ -0,0 +1,19 @@
+let add=10+20
+let sub=20-10
+let prod=10*20
+let quot=20/10
+let ramainder=9/2
+let expo=2 ** 4
+
+let myscore=100
+myscore++
+
+myscore--
+
+// comparison operator
+
+let num1=3
+let num2=3
+let num3=6
+
+console.log(num1==num2)
